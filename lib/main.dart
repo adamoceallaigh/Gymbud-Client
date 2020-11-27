3c5f717967ba27c435829b77a3bc521d3e636263
adamoc <oceallaighadam96@gmail.com>
2020-11-27 21:37:43 +0000

    Got the Users Displaying: FYP_Client Iteration 1 Result

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:Client/pages/loading.dart';
 import 'package:flutter/material.dart';
 
-// Setting up routes and paths for the flutter app
+// Setting up routes and paths for the flutter app  
 void main() => runApp(MaterialApp(
     initialRoute: '/',
     routes: {
