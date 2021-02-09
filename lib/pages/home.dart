2c4b61fafa87a17bf4d7fb9646f6d87b39ba137e
adamoc <oceallaighadam96@gmail.com>
2021-02-09 04:54:58 +0000

    Updated the homepage to display user profile image too

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,7 +4,7 @@ import 'package:Client/Helper_Widgets/hex_color.dart';
 import 'package:Client/Models/User.dart';
 import 'package:flutter/material.dart';
 
-///Setting my Page up as a Stateful Widget 
+///Setting my Page up as a Stateful Widget
 ///so I can change the state of the list of users
 
 class Home extends StatefulWidget {
@@ -15,16 +15,14 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<User> users = List<User>();
 
-  /// This function retrieves all users from the 
+  /// This function retrieves all users from the
   /// UserController and passes it to a state variable
   /// So you can make a ListView Builder out of it below
   void setupUsers() async {
     UserController userController = new UserController();
-    userController.getUsers().then((user) => 
-      setState((){
-        users.addAll(user);
-      })
-    );
+    userController.getUsers().then((user) => setState(() {
+          users.addAll(user);
+        }));
   }
 
   // Using Initiliazation method to set the state once with the list of users
@@ -36,110 +34,120 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Image.asset(
-              "Resources/Images/logoGymbud.png",
-              height: 100.0,
-              width: 100.0
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("Resources/Images/logoGymbud.png",
+            height: 100.0, width: 100.0),
+        backgroundColor: HexColor("FEFEFE"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Container(
+              color: Colors.white,
+              height: 50,
+              child: Center(
+                child: Text("Past Users / Sessions"),
+              ),
             ),
-            backgroundColor: HexColor("FEFEFE"),
           ),
-          body: Column(
-            children: <Widget>[
-              Card(
-                child: Container(
-                  color: Colors.white,
-                  height: 50,
-                  child: Center(
-                    child: Text("Past Users / Sessions"),
-                  ),
-                ),
-            ),
-            Expanded(
-              child: 
-              ListView.builder(
-                itemBuilder: (context , index){
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
                 return Card(
                   child: Padding(
-                    padding: const EdgeInsets.only(top:32.0 , bottom: 32.0 , left:16.0 , right:16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget> [
-                        Text(
-                          users[index].name,
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(bottom:15.0),
-                          child: Text(
-                            users[index].userName,
-                            style: TextStyle(
-                              color: HexColor("EB9661")
+                    padding: const EdgeInsets.only(
+                        top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
+                    child: Stack(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              users[index].name,
+                              style: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.bold),
                             ),
+                            Container(
+                              margin: EdgeInsets.only(bottom: 15.0),
+                              child: Text(
+                                users[index].userName,
+                                style: TextStyle(color: HexColor("EB9661")),
+                              ),
+                            ),
+                            Text(
+                              "Preferred Intensity: " +
+                                  users[index].preferredIntensity,
+                              // style: TextStyle(
+                              //   color: HexColor("EB9661")
+                              // ),
+                            ),
+                            Text(
+                              "Video / In Person: " +
+                                  users[index].videoOrInPerson,
+                              // style: TextStyle(
+                              //   color: HexColor("EB9661")
+                              // ),
+                            ),
+                            Text(
+                              "Fitness Level: " + users[index].fitnessLevel,
+                              // style: TextStyle(
+                              //   color: HexColor("EB9661")
+                              // ),
+                            ),
+                            Text(
+                              "Resources: " + users[index].resources.toString(),
+                              // style: TextStyle(
+                              //   color: HexColor("EB9661")
+                              // ),
+                            ),
+                          ],
+                        ),
+                        Positioned(
+                          top: -5,
+                          right: 0,
+                          child: CircleAvatar(
+                            radius: 50.0,
+                            backgroundImage: users[index].profile_url != null
+                                ? new NetworkImage(users[index].profile_url)
+                                : null,
+                            backgroundColor: Colors.transparent,
                           ),
-                        ),
-                        Text(
-                          "Preferred Intensity: " + users[index].preferredIntensity,
-                          // style: TextStyle(
-                          //   color: HexColor("EB9661")
-                          // ),
-                        ),
-                        Text(
-                          "Video / In Person: " + users[index].videoOrInPerson,
-                          // style: TextStyle(
-                          //   color: HexColor("EB9661")
-                          // ),
-                        ),
-                        Text(
-                          "Fitness Level: " + users[index].fitnessLevel,
-                          // style: TextStyle(
-                          //   color: HexColor("EB9661")
-                          // ),
-                        ),
-                        Text(
-                          "Resources: " + users[index].resources.toString(),
-                          // style: TextStyle(
-                          //   color: HexColor("EB9661")
-                          // ),
-                        ),
-                    ],
+                          // child: Container(
+
+                          //   padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          //   height: 100,
+                          //   width: 70,
+                          //   child: users[index].profile_url != null ? new Image.network(users[index].profile_url , fit: BoxFit.cover) :  null
+                          // )
+                        )
+                      ],
                     ),
-                ),
-              );
-            },
-            itemCount: users.length,
+                  ),
+                );
+              },
+              itemCount: users.length,
             ),
           ),
-            ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: HexColor("EB9661"),
-            currentIndex: 0, // this will be set when a new tab is tapped
-            items: [
-              BottomNavigationBarItem(
-                icon: Image.asset(
-                  "Resources/Images/House_Icon.png"
-                ),
-                label:'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Image.asset(
-                  "Resources/Images/Buds_Icon.png"
-                ),
-                label:'Buds',
-              ),
-              BottomNavigationBarItem(
-                icon: Image.asset(
-                  "Resources/Images/Calendar_Icon.png"
-                ),
-                label:'Calendar'
-              )
-            ],
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: HexColor("EB9661"),
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset("Resources/Images/House_Icon.png"),
+            label: 'Home',
           ),
-        );
-      }
-    }
+          BottomNavigationBarItem(
+            icon: Image.asset("Resources/Images/Buds_Icon.png"),
+            label: 'Buds',
+          ),
+          BottomNavigationBarItem(
+              icon: Image.asset("Resources/Images/Calendar_Icon.png"),
+              label: 'Calendar')
+        ],
+      ),
+    );
+  }
+}
