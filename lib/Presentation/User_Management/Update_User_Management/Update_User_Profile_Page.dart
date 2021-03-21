6de45b9031aeac936b05eca13cf2c837e74743cd
Adam O' Ceallaigh <oceallaighadam880@gmail.com>
2021-03-21 12:13:45 +0000

    Iteration 5.3 - Making Tracks , getting everything i have back working and working on features now

--- lib/Presentation/User_Management/Update_User_Management/Update_User_Profile_Page.dart
+++ lib/Presentation/User_Management/Update_User_Management/Update_User_Profile_Page.dart
@@ -4,19 +4,10 @@
 
 import 'package:Client/Helpers/GeneralHelperMethodManager.dart';
 import 'package:Client/Managers/Providers.dart';
-import 'package:Client/Presentation/User_Management/Read_User_Management/Read_User_Profile_Page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-// Page Imports
-import 'package:Client/Infrastructure/Models/User.dart';
-import 'package:Client/Helpers/ButtonProducer.dart';
-import 'package:Client/Helpers/GeneralNetworkingMethodManager.dart';
-import 'package:Client/Infrastructure/Models/InformationPopUp.dart';
 
 // Template to make the UpdateProfile View Page
 class UpdateProfilePage extends HookWidget {
@@ -30,23 +21,7 @@ class UpdateProfilePage extends HookWidget {
         GeneralHelperMethodManager(logged_in_user: logged_in_user);
 
     return Scaffold(
-        body: generalHelperMethodManager.getUpdateProfilePageBody());
+      body: generalHelperMethodManager.getUpdateProfilePageBody(),
+    );
   }
 }
-
-// import 'package:flutter_switch/flutter_switch.dart';
-// bool isProfileSettings = false;
-// FlutterSwitch(
-//   activeText: "Profile",
-//   inactiveText: "Activity Settings",
-//   value: isProfileSettings,
-//   valueFontSize: 10.0,
-//   width: 410,
-//   borderRadius: 30.0,
-//   showOnOff: true,
-//   onToggle: (val) {
-//     setState(() {
-//       isProfileSettings = val;
-//     });
-//   },
-// ),
